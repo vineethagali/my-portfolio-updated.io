334845acd8bc6ba3b37a57c64acfa8fb230d83bd
vineethagali <109171454+vineethagali@users.noreply.github.com>
2022-08-15 17:06:25 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -233,7 +233,7 @@
 					});
 
 				});
-<!--javascript for menu Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon --->
+// javascript for menu Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
