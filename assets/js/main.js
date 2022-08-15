718a9ae05999ced022e8cbb49070e0d91c39d038
vineethagali <109171454+vineethagali@users.noreply.github.com>
2022-08-15 16:48:32 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -233,6 +233,15 @@
 					});
 
 				});
+<!--javascript for menu Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon --->
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
 			// Hide intro on scroll (<= small).
 				breakpoints.on('<=small', function() {
